f75f8e41baa875434838d91eead367bad0b6c6d8
Mufutau Lamidi <damilarelamidi@gmail.com>
2020-06-21 15:38:02 +0100

    Modified for windows

--- app/src/main/java/com/example/shopitee/activities/MainActivity.kt
+++ app/src/main/java/com/example/shopitee/activities/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(getDatabasePath("word_databse").absolutePath)
         firebaseAuth = FirebaseAuth.getInstance()
         SystemClock.sleep(3000)
 
